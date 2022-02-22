79ec4a60831fdbf034b06c8e85b978c1e05a8da4
jhovanni223 <jhovanniplatt@gmail.com>
2022-02-22 15:27:46 -0600

    Practica 1 Ionic

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/Interface/user.modelo';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  formulario = this.nuevoFormualario.group({
+    nombre:'',
+    apellido:'',
+    email:'',
+    telefono:''
+});
 
+
+
+usuarios:User[]=[];
+constructor(private nuevoFormualario:FormBuilder){
+}
+
+
+CHILLSAVE(){
+        let nuevoUser : User = {
+            nombre :this.formulario.get('nombre')?.value,
+            apellido:this.formulario.get('apellido')?.value,
+            email:this.formulario.get('email')?.value,
+            telefono:this.formulario.get('telefono')?.value, 
+        }
+    
+
+        this.usuarios.push(nuevoUser);//PONER EN ARRAY
+
+        console.log(this.usuarios);
+        this.limpiarForm();
+}
+
+
+limpiarForm(){
+        this.formulario.reset()
+    }
 }
